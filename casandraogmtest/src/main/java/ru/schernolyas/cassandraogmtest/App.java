938553358ab8c1669cc8a13d2aed3de48f650470
Sergey Chernolyas <sergey_chernolyas@epam.com>
2017-02-14 18:13:43 +0300

    add logging

--- casandraogmtest/src/main/java/ru/schernolyas/cassandraogmtest/App.java
+++ casandraogmtest/src/main/java/ru/schernolyas/cassandraogmtest/App.java
@@ -8,24 +8,30 @@ package ru.schernolyas.cassandraogmtest;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import org.jboss.logging.Logger;
 
 /**
  *
  * @author Sergey Chernolyas
  */
 public class App {
+    private static Logger log = Logger.getLogger(App.class);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        log.info("Begin to work");
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("cassandra-ogm");
         EntityManager em = factory.createEntityManager();
 
         Person person = new Person("name1");
         em.getTransaction().begin();
         em.persist(person);
+        log.info("Begin to commit");
         em.getTransaction().commit();
+        log.info("finish");
+        System.exit(0);
     }
 
 }
